6b4a8e43c943fc3611142866757c5ae5623c6dde
Nikita Gannochka <ngannochka.js@gmail.com>
2025-10-21 21:04:13 +0700

    Подготовил сертификаты

--- app/components/Certificates.vue
+++ app/components/Certificates.vue
@@ -17,23 +17,21 @@ const certificates = [
     id="certificates"
     title="Сертификаты"
     :ui="{
-      title: 'text-left text-[#28445C]',
-      body: 'flex justify-center'
+      title: 'font-serif text-left text-[#28445C]'
     }"
   >
-    <!--    <template #body>-->
     <UCarousel
       v-slot="{ item }"
       dots
       :items="certificates"
       :ui="{
-        item: 'lg:basis-1/3'
+        item: 'sm:basis-1/2 md:basis-1/2 lg:basis-1/3',
+        dot: 'data-[state=active]:bg-[#28445C]'
       }"
+      class="w-full max-w-sm mx-auto sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl"
     >
-      <!--      class="w-full max-w-xs mx-auto"-->
       <NuxtImg :src="item" class="rounded-lg" />
     </UCarousel>
-    <!--    </template>-->
   </UPageSection>
 </template>
 
